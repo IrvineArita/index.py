c608cb8ff207d78dfc795eb93bfcaeac196b835d
IrvineArita <irvinemoraa2003@gmail.com>
2025-03-18 15:44:58 +0300

    initial commit

---
+++ dictionary.py
@@ -0,0 +1,7 @@
+capital_cities = {
+    "Kenya" : "Nairobi",
+    "Uganda" : "Kampala",
+}
+
+print(capital_cities)
+print(capital_cities["Kenya"])
---
+++ list.py
@@ -0,0 +1,3 @@
+employee_ids = {1, 1, 2, 4, 5, 8}
+print(employee_ids)
+employee_ids.add(89)
